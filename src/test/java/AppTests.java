fbd2f62ba2c275c78f7c017176d3470b2518fad3
Vasiliy Golban <vgolban@accesssoftek.com>
2021-03-25 22:10:10 +0200

    Create gradle project with one ui test

---
+++ src/test/java/AppTests.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppTests {
+
+    @BeforeAll
+    public static void BeforeAll() {
+        Configuration.baseUrl = "https://godzila.bg/en";
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void numberOfCitiesOnStart() {
+        open("");
+        $$(".popup-btn-city").shouldHaveSize(3);
+    }
+
+}
